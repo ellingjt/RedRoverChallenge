1b8aff78b7dc462d38611913749f463746081368
Josh Ellingson <jellingson@ostusa.com>
2026-08-18 21:24:45 -0500

    Base solution, updating gitignore

---
+++ RedRoverChallenge/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RedRoverChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
